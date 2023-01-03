dac3fe85a0388d761baacf97c7e5826177634130
myh333777 <myh333777@gmail.com>
2023-01-03 23:43:46 +0800

    移动index，本地可运行

--- local-run.sh
+++ local-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check for python-is-python3 installed
-if ! command -v python &>/dev/null; then
+if ! command -v python3 &>/dev/null; then
 	echo "It appears you do not have python-is-python3 installed"
 	exit 1
 fi
@@ -13,7 +13,7 @@ if ! command -v zola &>/dev/null; then
 fi
 
 # Check for correct slugify package
-PYTHON_ERROR=$(eval "python -c 'from slugify import slugify; print(slugify(\"Test String One\"))'" 2>&1)
+PYTHON_ERROR=$(eval "python3 -c 'from slugify import slugify; print(slugify(\"Test String One\"))'" 2>&1)
 
 if [[ $PYTHON_ERROR != "test-string-one" ]]; then
 	if [[ $PYTHON_ERROR =~ "NameError" ]]; then
@@ -25,7 +25,7 @@ if [[ $PYTHON_ERROR != "test-string-one" ]]; then
 fi
 
 # Check for rtoml package
-PYTHON_ERROR=$(eval "python -c 'import rtoml'" 2>&1)
+PYTHON_ERROR=$(eval "python3 -c 'import rtoml'" 2>&1)
 
 if [[ $PYTHON_ERROR =~ "ModuleNotFoundError" ]]; then
 	echo "It appears you do not have rtoml installed. Install it with 'pip install rtoml'"
@@ -34,16 +34,16 @@ fi
 
 # Check that the vault got set
 if [[ -z "${VAULT}" ]]; then
-	if [[ -f ".vault_path" ]]; then
-		export VAULT=$(cat .vault_path)
-	else
-		echo "Path to the obsidian vault is not set, please set the path using in the $(.vault_path) file or $VAULT env variable"
-		exit 1
-	fi
+	#if [[ -f ".vault_path" ]]; then
+		export VAULT="./content/Readlist"    #$(cat .vault_path)
+	#else
+	#	echo "Path to the obsidian vault is not set, please set the path using in the $(.vault_path) file or $VAULT env variable"
+	#	exit 1
+	#fi
 fi
 
 # Pull environment variables from the vault's netlify.toml when building (by generating env.sh to be sourced)
-python env.py
+python3 env.py
 
 # Set the site and repo url as local since locally built
 export SITE_URL=local
